db835f51719dd439e9ae612f6b5cd75bcbef140f
jay suthar <jaysuthar743@gmail.com>
2021-07-23 11:20:27 +0530

    added module hide archive unarchive action buttons

---
+++ hide_archive_unarchive_buttons/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hide_archive_unarchive_buttons",
+    'summary': """ Hide archive/unarchive buttons based on model """,
+    'description': """ Hide archive/unarchive buttons based on model """,
+    'author': "Jay Suthar",
+    'category': 'Customise',
+    'depends': ['base'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+}
